6b235a4da484a877b0bf0b724c45b46c98630dd6
atailkkan <atailkkan@gmail.com>
2022-10-09 10:48:44 +0300

    Uploaded files for PCAT

--- app.js
+++ app.js
@@ -1,3 +1,18 @@
-const fn = function() {
-    console.log('Function working...');
-};
+const path = require('path');
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/temp/index.html'));
+});
+
+
+
+
+
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Sunucu ${port} portunda başlatıldı.`);
+});
